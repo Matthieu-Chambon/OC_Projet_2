8dd2ec02a51298dcb43dc132ee95d67919bf6594
Matthieu <matthieu.chambon44@gmail.com>
2025-01-09 01:19:40 +0100

    ajout de commentaires

--- projet_2.py
+++ projet_2.py
@@ -1,3 +1,4 @@
+# Imports nécessaires au fonctionnement du programme
 import requests
 from bs4 import BeautifulSoup
 import csv
@@ -6,27 +7,29 @@ from urllib.parse import urljoin
 
 print("Phase 1")
 
+# Récupération de la page d'accueil du site "Books to Scrape" (Science Fiction)
 home_url = "https://books.toscrape.com/catalogue/category/books/science-fiction_16/index.html"
 home_page = requests.get(home_url)
 home_page.encoding = 'utf-8'
 home_soup = BeautifulSoup(home_page.content, 'html.parser')
 
+# Récupération de chaque livre
 articles = home_soup.find_all("article", class_="product_pod")
 
-f = open("article.txt", "w", encoding="utf-8")
-f.write(str(articles[1]))
-f.close()
-
-# print(article[0])
+# f = open("article.txt", "w", encoding="utf-8")
+# f.write(str(articles[1]))
+# f.close()
 
 file = "phase_1.csv"
 
+# Suppression du fichier phase_1.csv s'il existe afin de le regénérer
 if os.path.exists(file):
     os.remove(file)
     print(f"The file '{file}' has been deleted.")
 else:
     print(f"The file '{file}' does not exist.")
 
+# Ajout des en-têtes
 csv_headline = [
     "product_page_url",
     "universal_product_code",
@@ -39,12 +42,15 @@ csv_headline = [
     "review_rating",
     "image_url"]
 
+# Création du fichier .csv
 with open(file, mode="a", newline="", encoding="utf-8") as file:
     writer = csv.writer(file, delimiter=";")
     writer.writerow(csv_headline)
 
+    # Pour chaque livre (element <article>)
     for article in articles :
 
+        # Récupération de différentes données
         relative_url = article.find("h3").a["href"]
         product_page_url = urljoin(home_url, relative_url)
 
@@ -62,6 +68,7 @@ with open(file, mode="a", newline="", encoding="utf-8") as file:
         review_rating = book_soup.find("p", class_="star-rating")["class"][1]
         image_url = urljoin(product_page_url, book_soup.find("div", class_=['item', 'active']).find("img").src)
 
+        # Ecriture de ces données dans le fichier .csv
         writer.writerow([
             product_page_url,
             universal_product_code,
